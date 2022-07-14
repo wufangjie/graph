1d2287d088ece8535415de3db706e8d2ce8ae558
wfj <wufangjie1223@126.com>
2022-07-14 08:28:07 +0800

    use Option<W> to indicate distance, None means inf

--- src/bellman_ford.rs
+++ src/bellman_ford.rs
@@ -12,27 +12,32 @@ where
     /// unlike dijkstra iter, we will get start vertex in returned result
     /// so we should skip start vertex by hand
     /// TODO: a better way to init distance list: use inf (how?), or Option, or HashMap?
-    fn bellman_ford(&self, start: &Vertex<T>) -> (bool, Vec<W>, Vec<usize>) {
+    pub fn bellman_ford(&self, start: &Vertex<T>) -> (bool, Vec<Option<W>>, Vec<usize>) {
         let s = *self
             .v_map
             .get(start)
             .expect("Start vertex not in this graph");
         let n = self.len();
 
-        let mut dist = vec![Default::default(); n];
-        let mut from = vec![n; n]; // init n mean from noth
-        from[s] = s;
+        let mut dist = vec![None; n];
+        dist[s] = Some(Default::default());
+        let mut from = vec![s; n];
 
         for _ in 0..n {
             let mut improved = false;
             for u in 0..n {
                 for (&v, &w) in &self.e_lst[u] {
                     // so-called relax
-                    // the if condition is a bit of hard, a better way
-                    if from[u] != n && (from[v] == n || dist[u] + w < dist[v]) {
-                        from[v] = u;
-                        dist[v] = dist[u] + w;
-                        improved = true;
+                    if let Some(d) = dist[u] {
+                        let can_improve = match dist[v] {
+                            None => true,
+                            Some(d0) => d + w < d0,
+                        };
+                        if can_improve {
+                            from[v] = u;
+                            dist[v] = Some(d + w);
+                            improved = true
+                        }
                     }
                 }
             }
@@ -70,7 +75,7 @@ mod tests {
         println!("No negative cycle: {}:", s);
         for i in 0..n {
             println!(
-                "{}, dist: {}, directly from: {}",
+                "{}, dist: {:?}, directly from: {}",
                 &g2[i], dist[i], &g2[from[i]]
             )
         }
